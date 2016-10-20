3b603569f8faead17e9b2151e5a2d1c8230d1e21
Joel McCance <joelm@slalom.com>
2016-10-20 14:07:59 -0500

    Renamed types.

--- src/Hubot/Robot.purs
+++ src/Hubot/Robot.purs
@@ -1,89 +1,89 @@
 module Hubot.Robot (
   ROBOT
-  , RobotM()
-  , Robot()
   , Message
+  , Robot
+  , RobotConfig()
+  , RobotConfigM()
+  , withRobot
   , hear
   , listen
-  , withRobot
-  , Robot'
   ) where
 
 import Prelude
 import Control.Monad.Eff (Eff)
 
--- | The `ROBOT` effect indicates that an Eff action may access or modify a Hubot Robot.
+-- | The `ROBOT` effect indicates that an Eff action may access or modify a Hubot RobotConfig.
 foreign import data ROBOT :: !
 
 foreign import data Message :: *
-foreign import data Robot' :: *
+foreign import data Robot :: *
 
 -- BEGIN CARGO CULTING
 -- https://github.com/dancingrobot84/purescript-express/blob/0.4.0/src/Node/Express/App.purs#L27
-data RobotM e a = RobotM (Robot' -> Eff e a)
-type Robot e = RobotM (robot :: ROBOT | e) Unit
+data RobotConfigM e a = RobotConfigM (Robot -> Eff e a)
+type RobotConfig e = RobotConfigM (robot :: ROBOT | e) Unit
 
-instance functorRobotM :: Functor (RobotM e) where
-  map f (RobotM h) = RobotM \r -> liftM1 f $ h r
+instance functorRobotConfigM :: Functor (RobotConfigM e) where
+  map f (RobotConfigM h) = RobotConfigM \r -> liftM1 f $ h r
 
-instance applyRobotM :: Apply (RobotM e) where
-  apply (RobotM f) (RobotM h) = RobotM \robot -> do
+instance applyRobotConfigM :: Apply (RobotConfigM e) where
+  apply (RobotConfigM f) (RobotConfigM h) = RobotConfigM \robot -> do
     res <- h robot
     trans <- f robot
     pure $ trans res
 
-instance applicativeRobotM :: Applicative (RobotM e) where
-  pure x = RobotM \_ -> pure x
+instance applicativeRobotConfigM :: Applicative (RobotConfigM e) where
+  pure x = RobotConfigM \_ -> pure x
 
-instance bindRobotM :: Bind (RobotM e) where
-  bind (RobotM h) f = RobotM \robot -> do
+instance bindRobotConfigM :: Bind (RobotConfigM e) where
+  bind (RobotConfigM h) f = RobotConfigM \robot -> do
     res <- h robot
     case f res of
-      RobotM g -> g robot
+      RobotConfigM g -> g robot
 --END CARGO CULTING (mostly)
 
 --| Initialize a Hubot instance with your handlers.
-withRobot :: forall e a. RobotM e a -> Robot' -> Eff e a
-withRobot (RobotM f) = f
+withRobot :: forall e a. RobotConfigM e a -> Robot -> Eff e a
+withRobot (RobotConfigM f) = f
 
 --| Define a "hear" handler for Hubot.
 hear :: forall e a.
   String
   -> (Message -> a)
-  -> Robot e
-hear pattern handler = RobotM \robot ->
+  -> RobotConfig e
+hear pattern handler = RobotConfigM \robot ->
   _hear pattern handler robot
 
 --| Define a "listen" handler for Hubot.
 listen :: forall e a.
   (Message -> Boolean)
   -> (Message -> a)
-  -> Robot e
-listen matcher handler = RobotM \robot ->
+  -> RobotConfig e
+listen matcher handler = RobotConfigM \robot ->
   _listen matcher handler robot
 
 --| Define a "respond" handler for Hubot.
 respond :: forall e a.
   String
   -> (Message -> a)
-  -> Robot e
-respond pattern handler = RobotM \robot ->
+  -> RobotConfig e
+respond pattern handler = RobotConfigM \robot ->
   _respond pattern handler robot
 
 foreign import _hear :: forall e a.
   String
   -> (Message -> a)
-  -> Robot'
+  -> Robot
   -> (Eff (robot :: ROBOT | e) Unit)
 
 foreign import _listen :: forall e a.
   (Message -> Boolean)
   -> (Message -> a)
-  -> Robot'
+  -> Robot
   -> (Eff (robot :: ROBOT | e) Unit)
 
 foreign import _respond :: forall e a.
   String
   -> (Message -> a)
-  -> Robot'
+  -> Robot
   -> (Eff (robot :: ROBOT | e) Unit)
--- src/Main.purs
+++ src/Main.purs
@@ -5,8 +5,8 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (log)
 import Control.Monad.Eff.Unsafe (unsafePerformEff)
 
-import Hubot.Robot (ROBOT, Robot', withRobot, hear)
+import Hubot.Robot (ROBOT, Robot, withRobot, hear)
 
-main :: forall e. Robot' -> Eff (robot :: ROBOT | e) Unit
+main :: forall e. Robot -> Eff (robot :: ROBOT | e) Unit
 main = withRobot do
   hear "aaa" (\_ -> unsafePerformEff $ log "Heard asdf")
