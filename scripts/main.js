bed26610de29ad9aa36d94c015dc1ea56c22f4cd
Joel McCance <joelm@slalom.com>
2016-10-20 14:07:43 -0500

    Fixed deprecated doc warning.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,23 @@
-'use strict';
-
-var Main = require('../output/Main');
+// Description:
+//   <description of the scripts functionality>
+//
+// Dependencies:
+//   "<module name>": "<module version>"
+//
+// Configuration:
+//   LIST_OF_ENV_VARS_TO_SET
+//
+// Commands:
+//   hubot <trigger> - <what the respond trigger does>
+//   <trigger> - <what the hear trigger does>
+//
+// Notes:
+//   <optional notes required for the script>
+//
+// Author
+//    <github username of the original script author>
 
 module.exports = function (robot) {
+  var Main = require('../output/Main');
   Main.main(robot)();
 };
